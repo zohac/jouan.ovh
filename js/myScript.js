deeca546f352a36d75129887722f7ea2459fdea3
zohac <fenrir0680@gmail.com>
2018-09-22 16:07:23 +0200

    style(update): add animation to the carousel

--- js/myScript.js
+++ js/myScript.js
@@ -1,4 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+
+    $('.preloader-wrapper').detach();
 
     var elems = document.querySelectorAll('.carousel.carousel-slider');
     var options = {
@@ -14,25 +16,29 @@ document.addEventListener('DOMContentLoaded', function() {
     /**
      * Change the carousel an mousewheel
      */
-    $('body').on('mousewheel DOMMouseScroll', function( event ) {
-        if(typeof event.originalEvent.detail == 'number' && event.originalEvent.detail !== 0) {
-            handleMouseWheelDirection( -1 * event.originalEvent.detail );
+    $('body').on('mousewheel DOMMouseScroll', function (event) {
+        if (typeof event.originalEvent.detail == 'number' && event.originalEvent.detail !== 0) {
+            handleMouseWheelDirection(-1 * event.originalEvent.detail);
         } else if (typeof event.originalEvent.wheelDelta == 'number') {
-            handleMouseWheelDirection( event.originalEvent.wheelDelta );
+            handleMouseWheelDirection(event.originalEvent.wheelDelta);
         }
     });
-    
-    function handleMouseWheelDirection( event ) {
-        if ( event > 0 ) {
+
+    function handleMouseWheelDirection(event) {
+        if (event > 0) {
             // Scroll Up
             instance.prev();
             $('.carousel-slider').children().removeClass('right left').addClass('left');
-            activeIndicatorItem({nextId: -1});
-        } else if ( event < 0 ) {
+            activeIndicatorItem({
+                nextId: -1
+            });
+        } else if (event < 0) {
             // Scroll Down
             instance.next();
             $('.carousel-slider').children().removeClass('right left').addClass('right');
-            activeIndicatorItem({nextId: 1});
+            activeIndicatorItem({
+                nextId: 1
+            });
         }
     }
 
@@ -54,7 +60,7 @@ document.addEventListener('DOMContentLoaded', function() {
             if (activeEltId > carouselEltLength) {
                 activeEltId = 1;
             } else if (activeEltId < 1) {
-                activeEltId = carouselEltLength;            
+                activeEltId = carouselEltLength;
             }
         } else {
             activeEltId = activeIndicatorItem.selectedId;
@@ -66,10 +72,12 @@ document.addEventListener('DOMContentLoaded', function() {
     /**
      * Define the carousel element by clicking on the indicator item
      */
-    $('.indicator-item').on('click', function(){
+    $('.indicator-item').on('click', function () {
         var id = parseInt($(this).attr('id').split("-")[2]);
 
         instance.set(id - 1);
-        activeIndicatorItem({selectedId: id});
+        activeIndicatorItem({
+            selectedId: id
+        });
     });
-}); 
+});
