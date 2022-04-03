3ea2fbac18891ccd2dd297b78127a300b50395b8
zohac <simon@jouan.ovh>
2022-04-03 22:50:32 +0200

    refactor: refactor the application

---
+++ components/social/social-component.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <GithubComponent />
+    <TwitterComponent />
+    <LinkedinComponent />
+  </div>
+</template>
+
+<script>
+import GithubComponent from "@/components/social/github-component";
+import TwitterComponent from "@/components/social/twitter-component";
+import LinkedinComponent from "@/components/social/linkedin-component";
+export default {
+  name: "SocialComponent",
+  components: {LinkedinComponent, TwitterComponent, GithubComponent}
+}
+</script>
+
+<style scoped>
+
+</style>
