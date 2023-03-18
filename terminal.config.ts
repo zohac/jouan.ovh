5945b38f6cfbbf559064401154bae5988730fb3c
zohac <simon@jouan.ovh>
2023-03-18 19:20:45 +0100

    feat(terminal): add initial data in the terminal.config.ts file

--- terminal.config.ts
+++ terminal.config.ts
@@ -3,5 +3,13 @@ export const terminalDefaults = {
   height: "400px",
   userName: "anon.",
   domainName: "jouan.ovh",
-  initialData: "",
+  initialData:
+    "\xA0\xA0██╗███████╗\xA0\xA0\xA0\xA0\xA0██╗\xA0\xA0\xA0\xA0\xA0\xA0\xA0\xA0██╗██╗\xA0\xA0<br />" +
+    "\xA0██╔╝██╔════╝\xA0\xA0\xA0\xA0\xA0██║\xA0\xA0\xA0\xA0\xA0\xA0\xA0██╔╝╚██╗\xA0<br />" +
+    "██╔╝\xA0███████╗\xA0\xA0\xA0\xA0\xA0██║\xA0\xA0\xA0\xA0\xA0\xA0██╔╝\xA0\xA0╚██╗<br />" +
+    "╚██╗\xA0╚════██║██\xA0\xA0\xA0██║\xA0\xA0\xA0\xA0\xA0██╔╝\xA0\xA0\xA0██╔╝<br />" +
+    "\xA0╚██╗███████║╚█████╔╝\xA0\xA0\xA0\xA0██╔╝\xA0\xA0\xA0██╔╝\xA0<br />" +
+    "\xA0\xA0╚═╝╚══════╝\xA0╚════╝\xA0\xA0\xA0\xA0\xA0╚═╝\xA0\xA0\xA0\xA0╚═╝\xA0\xA0<br />" +
+    "<br />" +
+    'Bienvenue, pour voir les commandes disponible commencez par taper "help"<br />',
 };
