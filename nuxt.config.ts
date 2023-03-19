20e0789b258e2faede5c5e20f17d6a85fa07bd68
zohac <simon@jouan.ovh>
2023-03-19 19:35:11 +0100

    update github workflow

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+// import { defineNuxtConfig } from "@nuxt/bridge";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
@@ -13,15 +15,35 @@ export default defineNuxtConfig({
         {
           name: "description",
           content:
-            "Développeur Fullstack et Testeur/QA freelance passionné par la création de solutions web performantes. Discutons de votre projet.",
+            "Développeur Fullstack et Testeur/QA freelance passionné par la création de solutions web performantes." +
+            " Discutons de votre projet.",
         },
       ],
     },
-    baseURL: "jouan.ovh",
+    // baseURL: "jouan.ovh",
   },
   css: ["@/assets/scss/main.scss"],
   modules: ["@nuxt/content", "@nuxt/image-edge"],
   ssr: true,
+  target: "static",
+  experimental: {
+    payloadExtraction: false,
+  },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      routes: ["/", "/**"],
+    },
+    routeRules: {
+      "/": { static: true },
+      "/**": { static: true },
+    },
+  },
+  vite: {
+    build: {
+      ssr: true,
+    },
+  },
   webpack: {
     extractCSS: true,
   },
