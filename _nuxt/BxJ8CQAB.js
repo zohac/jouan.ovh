8b5a5d921a941e60a9e46434de697eceb1c5e7cc
zohac <zohac@users.noreply.github.com>
2026-09-17 10:25:27 +0000

    deploy: 6cfa23a3c0cf96decd433f07c13e95d4429a510a

--- _nuxt/BxJ8CQAB.js
+++
@@ -1,1 +0,0 @@
-import{_ as e}from"./DlAUqK2U.js";import{o as r,c as o}from"./Caa_y_bG.js";const c={};function s(t,n){return r(),o("hr")}const f=Object.assign(e(c,[["render",s]]),{__name:"ProseHr"});export{f as default};
---
+++ _nuxt/DW6KjCsN.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./CEsKezdT.js";import{o as s,s as n,w as f,x as p}from"./SjLJ41Sd.js";const l={__name:"ProseA",props:{href:{type:String,default:""},target:{type:String,default:void 0,required:!1}},setup(e){const t=e;return(r,_)=>{const o=a;return s(),n(o,{href:t.href,target:t.target},{default:f(()=>[p(r.$slots,"default")]),_:3},8,["href","target"])}}};export{l as default};
